7743a29ae3657d3c0cdf70a64cf5006553aa1460
18703508633 <1092231350@qq.com>
2021-12-14 14:32:53 +0800

    。

--- src/pages/system/basics/whole.vue
+++ src/pages/system/basics/whole.vue
@@ -1,12 +1,48 @@
 <template>
-    <div>1</div>
+  <div class="whole-container">
+    <div style="width: 1100" class="paging">
+      <a-tabs default-active-key="1" :tab-position="tabPosition">
+        <a-tab-pane key="1" tab="基本">
+          Content of Tab 1
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="会员">
+          Content of Tab 2
+        </a-tab-pane>
+        <a-tab-pane key="3" tab="充值">
+          Content of Tab 3
+        </a-tab-pane>
+         <a-tab-pane key="4" tab="邮件">
+          Content of Tab 3
+        </a-tab-pane>
+         <a-tab-pane key="5" tab="短信">
+          Content of Tab 3
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-}
+  data() {
+    return {
+      tabPosition: "left"
+    };
+  },
+  methods: {
+    callback(val) {
+      console.log(val);
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
-
+.whole-container {
+  background: #fff;
+  padding: 20px;
+  .content {
+    width: 600px;
+  }
+}
 </style>
