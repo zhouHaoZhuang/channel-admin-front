d00f38aef50961bd125efe103f3342ca11180421
18703508633 <1092231350@qq.com>
2021-12-14 15:13:02 +0800

    。

--- src/pages/system/basics/basic.vue
+++ src/pages/system/basics/basic.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>基本</div>
+  <div class="basic-container">
+    <div class="content">
+      <a-collapse default-active-key="1" :bordered="false">
+        <a-collapse-panel key="1" header="This is panel header 1">
+          <p>{{ text }}</p>
+        </a-collapse-panel>
+        <a-collapse-panel
+          key="2"
+          header="This is panel header 2"
+          :disabled="false"
+        >
+          <p>{{ text }}</p>
+        </a-collapse-panel>
+        <a-collapse-panel key="3" header="This is panel header 3">
+          <p>{{ text }}</p>
+        </a-collapse-panel>
+      </a-collapse>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-   
-}
+export default {};
 </script>
 
 <style lang="less" scoped>
-
+.content{
+    padding-top: 10px;
+}
 </style>
--- src/pages/system/basics/whole.vue
+++ src/pages/system/basics/whole.vue
@@ -3,19 +3,24 @@
     <div style="width: 1100" class="paging">
       <a-tabs default-active-key="1" :tab-position="tabPosition">
         <a-tab-pane key="1" tab="基本">
-          Content of Tab 1
+          <div class="big-block-title">基本</div>
+          <Basic/>
         </a-tab-pane>
         <a-tab-pane key="2" tab="会员">
-          Content of Tab 2
+          <div class="big-block-title">会员</div>
+          <Member/>
         </a-tab-pane>
         <a-tab-pane key="3" tab="充值">
-          Content of Tab 3
+          <div class="big-block-title">充值</div>
+          <Topup/>
         </a-tab-pane>
-         <a-tab-pane key="4" tab="邮件">
-          Content of Tab 3
+        <a-tab-pane key="4" tab="邮件">
+          <div class="big-block-title">邮件</div>
+          <Mail/>
         </a-tab-pane>
-         <a-tab-pane key="5" tab="短信">
-          Content of Tab 3
+        <a-tab-pane key="5" tab="短信">
+          <div class="big-block-title">短信</div>
+          <Note/>
         </a-tab-pane>
       </a-tabs>
     </div>
@@ -23,11 +28,20 @@
 </template>
 
 <script>
+import Basic from './basic.vue'
+import Member from './member.vue'
+import Topup from './topup.vue'
+import Mail from './mail.vue'
+import Note from './note.vue'
+
 export default {
   data() {
     return {
       tabPosition: "left"
     };
+  },
+  components:{
+      Basic,Member,Topup,Mail,Note
   },
   methods: {
     callback(val) {
@@ -43,6 +57,10 @@ export default {
   padding: 20px;
   .content {
     width: 600px;
+  }
+  .big-block-title {
+    color: #292929;
+    font-size: 18px;
   }
 }
 </style>
