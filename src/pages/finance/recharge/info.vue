943506de990291abfebe758c07448f0c2cbb5434
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2021-12-23 19:07:18 +0800

    格式化文档

--- src/pages/finance/recharge/info.vue
+++ src/pages/finance/recharge/info.vue
@@ -3,23 +3,45 @@
        v-if="data">
     <h1 class="details-title">交易信息</h1>
     <div class="details-info">
-      <div><span class="details-type">充值ID：</span><span class="details-value">{{data.id}}</span></div>
-      <div><span class="details-type">充值方式：</span><span class="details-value">{{data.memo}}</span></div>
-      <div><span class="details-type">充值金额：</span><span class="details-value">{{data.amount}}</span></div>
-      <div><span class="details-type">充值时间：</span><span class="details-value">{{data.payTime}}</span></div>
-      <div><span class="details-type">充值状态：</span><span class="details-value">{{data.status}}</span></div>
-      <div><span class="details-type">充值渠道：</span><span class="details-value">{{data.channelCode  }}</span></div>
-      <div><span class="details-type">操作人：</span><span class="details-value">{{data.createUserName}}</span></div>
+      <div>
+        <span class="details-type">充值ID：</span><span class="details-value">{{data.id}}</span>
+      </div>
+      <div>
+        <span class="details-type">充值方式：</span><span class="details-value">{{data.memo}}</span>
+      </div>
+      <div>
+        <span class="details-type">充值金额：</span><span class="details-value">{{data.amount}}</span>
+      </div>
+      <div>
+        <span class="details-type">充值时间：</span><span class="details-value">{{data.payTime}}</span>
+      </div>
+      <div>
+        <span class="details-type">充值状态：</span><span class="details-value">{{data.status}}</span>
+      </div>
+      <div>
+        <span class="details-type">充值渠道：</span><span class="details-value">{{data.channelCode  }}</span>
+      </div>
+      <div>
+        <span class="details-type">操作人：</span><span class="details-value">{{data.createUserName}}</span>
+      </div>
     </div>
     <div class="placeholder">
 
     </div>
     <h1 class="details-title">会员信息</h1>
     <div class="details-info">
-      <div><span class="details-type">会员ID：</span><span class="details-value">{{data.corporationCode}}</span></div>
-      <div><span class="details-type">会员姓名：</span><span class="details-value">{{data.realName}}</span></div>
-      <div><span class="details-type">手机号码：</span><span class="details-value">{{data.phoneNumber}}</span></div>
-      <div><span class="details-type">邮箱：</span><span class="details-value">{{data.email}}</span></div>
+      <div>
+        <span class="details-type">会员ID：</span><span class="details-value">{{data.corporationCode}}</span>
+      </div>
+      <div>
+        <span class="details-type">会员姓名：</span><span class="details-value">{{data.realName}}</span>
+      </div>
+      <div>
+        <span class="details-type">手机号码：</span><span class="details-value">{{data.phoneNumber}}</span>
+      </div>
+      <div>
+        <span class="details-type">邮箱：</span><span class="details-value">{{data.email}}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -52,6 +74,7 @@ export default {
   margin: 10px 20px;
   background-color: #fff;
   padding-bottom: 20px;
+
   .details-title {
     border-bottom: 1px solid #ebebeb;
     padding-left: 32px;
@@ -63,19 +86,23 @@ export default {
     background-color: #fff;
     color: #292929;
   }
+
   .placeholder {
     height: 20px;
     background-color: #f0f2f5;
   }
+
   .details-info {
     display: flex;
     margin-left: 32px;
     width: 100%;
     flex-wrap: wrap;
+
     div {
       width: 32%;
       height: 32px;
     }
+
     .details-type {
       display: inline-block;
       line-height: 24px;
@@ -86,6 +113,7 @@ export default {
       font-size: 12px;
       padding: 4px 0;
     }
+
     .details-value {
       display: inline-block;
       line-height: 24px;
