97a0b68f338aa108e944c03d26bcca7b488ddaa0
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2021-12-24 10:16:34 +0800

    修改请求

--- src/store/modules/financialDetails.js
+++ src/store/modules/financialDetails.js
@@ -22,8 +22,9 @@ const financialDetails = {
     },
     selectList ({ commit, state }, params) {
       return request({
-        url: `/customerAccountLog?key=createTime&search=${params.startTime}&currentPage=1&pageSize=10&total=0&qp-createTime-gt=${params.startTime}&qp-createTime-lt=${params.endTime}`,
+        url: `/customerAccountLog`,
         method: "get",
+        params,
         // pay:true
       });
     },
