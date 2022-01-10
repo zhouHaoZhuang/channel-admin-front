81bd37a886729b1cd2a51fd1369f75fc21cd027d
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2022-01-10 18:52:43 +0800

    修改

--- src/utils/enum.js
+++ src/utils/enum.js
@@ -1,43 +1,43 @@
 // 渠道价格折扣方式
 export const channelPriceType = {
   "0": "固定价格",
-  "1": "比例折扣"
+  "1": "比例折扣",
 };
 // 企业列表企业认证状态
 export const certificationStatusEnum = {
   0: "已认证",
-  1: "未认证"
+  1: "未认证",
 };
 // 企业列表企业状态
 export const corporationStatusEnum = {
   0: "正常",
-  1: "冻结"
+  1: "冻结",
 };
 // 地域map数据
 export const regionMapData = {
   "cn-hangzhou": "杭州",
   "cn-dalian": "大连",
   "cn-beijing": "北京",
-  "cn-shanghai": "上海"
+  "cn-shanghai": "上海",
 };
 //续费降配列表状态
 export const renewalStatusEnum = {
   1: "待降配",
-  2: "已完成"
+  2: "已完成",
 };
 // 充值状态map数据
 export const detailsMapData = {
   "0": "待支付",
   "1": "已取消",
   "2": "支付失败",
-  "3": "支付完成"
+  "3": "支付完成",
 };
 // 处理状态map数据
 export const detailTypeMapData = {
   "0": "待审核",
   "1": "待充值",
   "2": "支付失败",
-  "9": "支付完成"
+  "9": "支付完成",
 };
 
 // 款项类型map数据
@@ -45,19 +45,34 @@ export const paymentTypeMapData = {
   "1": "在线充值",
   "2": "线下充值",
   "3": "下单",
-  "4": "退款"
+  "4": "退款",
 };
-
+// 订单状态
+export const orderStatusEnum = {
+  "-3": "服务器创建异常",
+  "-1": "交易关闭",
+  1: "待支付",
+  3: "支付中",
+  5: "交易完成",
+  9: "支付完成",
+};
 // 会员折扣方式
 export const memberDiscountType = {
   "0": "固定价格",
-  "1": "比例折扣"
+  "1": "比例折扣",
 };
-
+export const OrderTypeMap = {
+  "1": "资源池新购",
+  "5": "阿里云采购",
+  "15": "资源池续费",
+  "16": "阿里云续费",
+  "25": "资源池升配",
+  "26": "阿里云升配",
+};
 // 权限管理-权限单选map
 export const systemAdminMapEnum = {
   DATA: "数据",
   // API: "API",
   MENU: "菜单",
-  BUTTON: "按钮"
+  BUTTON: "按钮",
 };
