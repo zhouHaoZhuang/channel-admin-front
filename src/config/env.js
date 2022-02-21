aeed495f0185d2c853a1ee3af1687e0d4cabd586
韩启航 <1878806686@qq.com>
2022-02-21 13:51:35 +0800

    修改

--- src/config/env.js
+++ src/config/env.js
@@ -12,7 +12,7 @@ const jadepoolUrl = {
 };
 // form服务接口请求地址
 const formBaseUrl = {
-  dev: "http://www.ydidc.com/server/form",
+  dev: "http://site.ydidc.com/server/form",
   test: "http://www.zjyundun.com/server/form",
   prod: "http://www.zjyundun.com/server/form"
 };
