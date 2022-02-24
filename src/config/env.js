325ee94f1d9a4cb76049e12c83b2d00df8b7c09f
韩启航 <1878806686@qq.com>
2022-02-24 15:34:50 +0800

    调整配置

--- src/config/env.js
+++ src/config/env.js
@@ -20,26 +20,10 @@ const formBaseUrl = {
 const domains = {
   dev: "localhost"
 };
-// authing接口参数
-const authingReq = {
-  dev: {
-    appId: "619c93dc69a93fbb8a1faf8c",
-    appHost: "https://resource-poor.authing.cn"
-  },
-  test: {
-    appId: "61cc1320c33bcb838eafe6cb",
-    appHost: "https://idc.authing.cn"
-  },
-  prod: {
-    appId: "",
-    appHost: ""
-  }
-};
 
 export default {
   BASE_URL: baseUrl[process.env.VUE_APP_ENV],
   JADE_POOL_URL: jadepoolUrl[process.env.VUE_APP_ENV],
   FORM_BASE_URL: formBaseUrl[process.env.VUE_APP_ENV],
-  DOMAIN_URL: domains[process.env.VUE_APP_ENV],
-  ...authingReq[process.env.VUE_APP_ENV]
+  DOMAIN_URL: domains[process.env.VUE_APP_ENV]
 };
