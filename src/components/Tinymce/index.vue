08b4252acc8f6327339d5cddfd49c7385271cec7
韩启航 <1878806686@qq.com>
2022-03-16 11:50:29 +0800

    调整富文本版本

--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -7,6 +7,7 @@
 <script>
 import tinymce from "tinymce/tinymce";
 import editor from "@tinymce/tinymce-vue";
+import "tinymce/icons/default";
 import "tinymce/plugins/contextmenu";
 import "tinymce/plugins/colorpicker";
 import "tinymce/plugins/textcolor"; // 文字颜色
@@ -69,31 +70,10 @@ export default {
         plugins:
           "lists image code table wordcount charmap codesample fullscreen insertdatetime preview print save searchreplace tabfocus textpattern toc visualblocks visualchars", // 插件
         toolbar:
-          "formats undo redo | paste print | preview fullscreen | image | spellchecker searchreplace | fontsizeselect | fontselect | styleselect | wordcount ltr rtl visualchars visualblocks toc | pagebreak nonbreaking|outdent indent aligncenter alignleft alignright alignjustify lineheight  underline quicklink h2 h3 blockquote numlist bullist table removeformat forecolor backcolor bold italic  strikethrough hr charmap link insertdatetime|subscript superscript cut codesample code",
+          "formats undo redo | paste print | preview fullscreen | image | spellchecker searchreplace | fontsizeselect | fontselect | wordcount ltr rtl visualchars visualblocks toc | pagebreak nonbreaking|outdent indent aligncenter alignleft alignright alignjustify lineheight  underline quicklink h2 h3 blockquote numlist bullist table removeformat forecolor backcolor bold italic  strikethrough hr charmap link insertdatetime|subscript superscript cut codesample code",
         fontsize_formats: "12px 14px 16px 18px 20px 24px", // 字号
-        letterspacing_formats:
-          "0 0.5pt 1pt 1.5pt 2pt 3pt 5pt 10pt 15pt 20pt 30pt", // 字间距
-        style_formats: [
-          {
-            title: "行高",
-            items: [
-              { title: "1", block: "p", styles: { "line-height": "1.0" } },
-
-              { title: "1.5", block: "p", styles: { "line-height": "1.5" } },
-
-              { title: "1.75", block: "p", styles: { "line-height": "1.75" } },
-
-              { title: "2", block: "p", styles: { "line-height": "2" } },
-
-              { title: "3", block: "p", styles: { "line-height": "3" } },
-
-              { title: "4", block: "p", styles: { "line-height": "4" } },
-
-              { title: "5", block: "p", styles: { "line-height": "5" } }
-            ]
-          }
-        ],
         branding: false, // 去水印
+        toolbar_sticky: true,
         // 增加下面的images_upload_handler对象，能够支持上传图片到服务器
         images_upload_handler: (blobInfo, success, failure) => {
           if (blobInfo.blob().size / 1024 / 1024 > 5) {
